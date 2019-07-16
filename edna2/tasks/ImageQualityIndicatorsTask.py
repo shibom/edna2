d644b19dbd89bc743be9ea5cd997acabe10306eb
Shibom Basu <shibom.basu@esrf.fr>
2019-07-16 17:50:48 +0200

    exec_crystfel tr6

--- edna2/tasks/ImageQualityIndicatorsTask.py
+++ edna2/tasks/ImageQualityIndicatorsTask.py
@@ -33,7 +33,7 @@ import time
 import numpy
 import base64
 import pathlib
-import json
+
 
 from tasks.AbstractTask import AbstractTask
 from tasks.WaitFileTask import WaitFileTask
@@ -334,12 +334,12 @@ class ImageQualityIndicatorsTask(AbstractTask):
                 listImageQualityIndicators += listImageDozor
 
                 # a work around as autocryst module works with only json file/string
-                inDataCrystFEL = json.dumps({'imageQualityIndicators': listImageDozor})
+                inDataCrystFEL = {'imageQualityIndicators': listImageDozor}
                 crystfel = ExeCrystFEL(inData=inDataCrystFEL)
                 cryst_result_out = crystfel.run(inDataCrystFEL)
                 if not crystfel.isFailure():
                     listcrystfel_output.append(cryst_result_out)
-                    listImageQualityIndicators += listcrystfel_output
+                    # listImageQualityIndicators += listcrystfel_output
 
 
         #                     xsDataImageQualityIndicators.dozorSpotsIntAver = imageDozor.spotsIntAver
