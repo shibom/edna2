394ca749c082c43834736b31976b2fdf77732c6f
Shibom Basu <shibom.basu@esrf.fr>
2019-07-19 14:13:05 +0200

    crystfel task UtilsLogging

--- edna2/tasks/CrystfelTasks.py
+++ edna2/tasks/CrystfelTasks.py
@@ -1,3 +1,24 @@
+#
+# Copyright (c) European Synchrotron Radiation Facility (ESRF)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
 from __future__ import division, print_function
 import os
 import sys
@@ -10,22 +31,14 @@ import lib.autocryst.src.saveDozor as sd
 from lib.autocryst.src.crystfel_utils import Utils
 
 from tasks.AbstractTask import AbstractTask
+from utils import UtilsLogging
 
-'''
-from tasks.H5ToCBFTask import H5ToCBFTask
-from tasks.ReadImageHeader import ReadImageHeader
-from tasks.ISPyBTasks import ISPyBRetrieveDataCollection
-
-from utils import UtilsPath
-from utils import UtilsImage
-from utils import UtilsConfig
-'''
 
 __author__ = ['S. Basu']
 __license__ = 'MIT'
 __date__ = '05/07/2019'
 
-logger = logging.getLogger('autoCryst')
+logger = UtilsLogging.getLogger()
 
 
 class ExeCrystFEL(AbstractTask):
@@ -124,12 +137,12 @@ class ExeCrystFEL(AbstractTask):
                     cryst.status = False
                     self.setFailure()
             except Exception as err:
-                logger.info("Error-crystfel:{}".format(err))
+                logger.error("Error-crystfel:{}".format(err))
                 dd.success = False
                 self.setFailure()
 
         else:
-            logger.info("something not correct with ImageQualityIndicator parsing")
+            logger.error("something not correct with ImageQualityIndicator parsing")
             dd.success = False
         return outData
 
@@ -168,7 +181,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(name)-12s %(levelname)-8s%(message)s',
                         datefmt='%y-%m-%d %H:%M',
-                        filename='autoCryst.log',
+                        filename='autocryst.log',
                         filemode='a+')
     fh = open(sys.argv[1], 'r')
     inData = json.load(fh)
